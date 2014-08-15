47bfd9668b386a3f10e25a8516ba93b8ea59e1b5
Kent Fredric <kentfredric@gmail.com>
2014-08-16 09:24:14 +1200

    Reduce whitespace

--- lib/Dist/Zilla/PluginBundle/Author/KENTNL/Lite.pm
+++ lib/Dist/Zilla/PluginBundle/Author/KENTNL/Lite.pm
@@ -13,59 +13,6 @@ use Class::Load 0.06 qw( load_optional_class load_class );
 
 # AUTHORITY
 
-=head1 SYNOPSIS
-
-    -[@Author::KENTNL]
-    +[@Author::KENTNL::Lite]
-
-    dzil build
-    dzil test
-    dzil release # BANG.
-
-=head1 NAMING SCHEME
-
-Please read my rant in L<Dist::Zilla::PluginBundle::Author::KENTNL/NAMING SCHEME> about the Author:: convention.
-
-=cut
-
-=head1 DESCRIPTION
-
-This is an attempt at one way of solving a common problem when contributing to things built with
-L<< C<Dist::Zilla>|Dist::Zilla >>.
-
-This is done by assuming that the code base that its targeting will B<NEVER> be released in its built form,
-but close enough to the normal build method that it's suitable for testing and contributing.
-
-=over 4
-
-=item * Less install time dependencies
-
-=item * More phases in the C<PluginBundle> generation are 'optional'
-
-=item * Less points of failure
-
-=back
-
-Good examples of things I've experienced in this category are the 2 following ( But awesome ) plug-ins that I use everywhere.
-
-=head2 L<< The C<::Git> Plug-ins|Dist::Zilla::Plugin::Git >>
-
-These plug-ins are great, don't get me wrong, but they pose a barrier for people on Win32, and in fact, anyone without a copy of
-Git installed, ( Its hard enough getting a copy of the pre-release source without Git, but that's available in C<tar.gz> and
-C<.zip> on C<github> ).
-
-Working Copies of Git plug-ins are also nonessential if you're not building releases.
-
-=head2 L<< The C<::Twitter> Plug-in|Dist::Zilla::Plugin::Twitter >>
-
-Also, a handy plug-in to have, but you're not going to be needing it unless you're tweeting a release, and usually,
-that means you're me.
-
-Some of its dependencies have been known to fail tests on Windows platforms, and thus block automatic installation, so seeing
-you don't have any use for this, its sensible to leave it out.
-
-=cut
-
 use Moose qw( with );
 
 with 'Dist::Zilla::Role::PluginBundle';
@@ -276,3 +223,55 @@ no Moose;
 ## no critic (RequireEndWithOne)
 'Thankyou for flying with KENTNL Lite!';
 
+=head1 SYNOPSIS
+
+    -[@Author::KENTNL]
+    +[@Author::KENTNL::Lite]
+
+    dzil build
+    dzil test
+    dzil release # BANG.
+
+=head1 NAMING SCHEME
+
+Please read my rant in L<Dist::Zilla::PluginBundle::Author::KENTNL/NAMING SCHEME> about the Author:: convention.
+
+=cut
+
+=head1 DESCRIPTION
+
+This is an attempt at one way of solving a common problem when contributing to things built with
+L<< C<Dist::Zilla>|Dist::Zilla >>.
+
+This is done by assuming that the code base that its targeting will B<NEVER> be released in its built form,
+but close enough to the normal build method that it's suitable for testing and contributing.
+
+=over 4
+
+=item * Less install time dependencies
+
+=item * More phases in the C<PluginBundle> generation are 'optional'
+
+=item * Less points of failure
+
+=back
+
+Good examples of things I've experienced in this category are the 2 following ( But awesome ) plug-ins that I use everywhere.
+
+=head2 L<< The C<::Git> Plug-ins|Dist::Zilla::Plugin::Git >>
+
+These plug-ins are great, don't get me wrong, but they pose a barrier for people on Win32, and in fact, anyone without a copy of
+Git installed, ( Its hard enough getting a copy of the pre-release source without Git, but that's available in C<tar.gz> and
+C<.zip> on C<github> ).
+
+Working Copies of Git plug-ins are also nonessential if you're not building releases.
+
+=head2 L<< The C<::Twitter> Plug-in|Dist::Zilla::Plugin::Twitter >>
+
+Also, a handy plug-in to have, but you're not going to be needing it unless you're tweeting a release, and usually,
+that means you're me.
+
+Some of its dependencies have been known to fail tests on Windows platforms, and thus block automatic installation, so seeing
+you don't have any use for this, its sensible to leave it out.
+
+=cut
